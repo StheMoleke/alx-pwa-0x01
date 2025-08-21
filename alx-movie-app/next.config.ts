d5a131d6dcdc289f51a50e02a2772f5df850b4b0
sthembiso <stherabite9@gmail.com>
2025-08-21 21:47:09 +0200

    Configure next-pwa in Next.js

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,7 +1,17 @@
+import withPWAInit from "@ducanh2912/next-pwa";
 import type { NextConfig } from "next";
 
+const withPWA = withPWAInit({
+  dest: "public", // where the service worker + manifest will be generated
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"], // allow movie posters from IMDb
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+});
